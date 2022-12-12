076ff916a59a3ac359c6a8cc33d8c80f4b9cb6cd
Praveen V Tavare <praveentavare@gmail.com>
2022-12-12 12:43:49 +0530

    Main class chnages for Thread example

--- course/src/main/java/com/example/course/corejava/MainClass.java
+++ course/src/main/java/com/example/course/corejava/MainClass.java
@@ -4,10 +4,15 @@ public class MainClass {
 
 	public static void main(String[] args) {
 
+		// Thread task created using Runnable interface
 		ThreadClass1 th1 = new ThreadClass1();
 		Thread thread = new Thread(th1);
 
 		thread.start();
+		
+		// Thread task created using Thread class
+		ThreadClass2 th2 = new ThreadClass2();
+		th2.start();
 	}
 
 }
